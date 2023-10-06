187e3982e0708d1a5fff8191fa04efcc787813de
Nirmit Arora <92266575+i-was-here@users.noreply.github.com>
2023-10-06 22:50:02 +0530

    Update inf.py

--- inf.py
+++ inf.py
@@ -4,22 +4,26 @@ from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from diffusers.models import AutoencoderKL
 from PIL import Image
 import numpy as np
-model_id = "stabilityai/stable-diffusion-2"
-scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler", torch_dtype=torch.float16)
-vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse", torch_dtype=torch.float16)
 out_img = Image.new("RGB", (768, 768), color="black")
-pipe = StableDiffusionPipeline.from_pretrained(model_id,
-                                               scheduler=scheduler,
-                                               vae=vae,
-                                              #  safety_checker=safe,
-                                               revision="fp16",
-                                               torch_dtype=torch.float16)
-pipe.enable_xformers_memory_efficient_attention()
-pipe = pipe.to("cuda")
 
 def prompt_to_image(prompt):
+  model_id = "stabilityai/stable-diffusion-2"
+  scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler", torch_dtype=torch.float16)
+  vae = AutoencoderKL.from_pretrained("stabilityai/sd-vae-ft-mse", torch_dtype=torch.float16)
+  pipe = StableDiffusionPipeline.from_pretrained(model_id,
+                                                 scheduler=scheduler,
+                                                 vae=vae,
+                                                #  safety_checker=safe,
+                                                 revision="fp16",
+                                                 torch_dtype=torch.float16)
+  pipe.enable_xformers_memory_efficient_attention()
+  pipe = pipe.to("cuda")
   results = pipe(prompt, height=768, width=768, guidance_scale = 10)
   if not results.nsfw_content_detected[0]: return results.images[0]
+  else: return out_img
+
+def get_image(res):
+  if not res.nsfw_content_detected[0]: return res.images[0]
   else: return out_img
 
 import gradio as gr
